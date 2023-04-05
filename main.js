6f58fe6b2ffd4979a12aaffc9fc1aa4a778bf4e2
Jeffchua96 <jeffersonchua@pursuit.org>
2023-04-04 21:06:00 -0400

    omg test

--- main.js
+++ main.js
@@ -7,8 +7,17 @@
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
- // Add code you want to run on page load here
+    fetch(("https://resource-ghibli-api.onrender.com")
+    .then(response => response.text())
+    .then(data => {
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(data, "application/xml");
+      console.log(xml);
+    })
+    .catch(console.error);
+ 
 }
+
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
 // So that testing can work as expected for now
